74cb04cf0c7e063e526bb4d48cff1bfe394267f9
Alessandro Perri <alessandro_perri@outlook.it>
2022-05-31 23:49:57 +0200

    comments

--- python_statistics/statistics.py
+++ python_statistics/statistics.py
@@ -4,6 +4,10 @@ from sr.robot import *
 import math
 
 class Analiser:
+	#Args: robot (Robot): instance of Robot class
+	#      n_tokens (int): number of tokens in the circuit
+	#      script (int): integer used to distinguish between the two (or more) scripts
+	#      occurrence (int): integer used to distinguish between the i-th occurrence of each script	
 	def __init__(self,robot,n_tokens,script,occurrence):
 		self.robot=robot
 		self.f = open("stats/distances"+str(script)+str(occurrence)+".txt", "w")
@@ -15,14 +19,16 @@ class Analiser:
 		self.dist_right=100
 		self.n_tokens=n_tokens
 		self.c=0
-		
+
+	#function to write left, frontal and right closest distance to a .txt file	
 	def collect_distances(self):
 		self.find_obstacles()
 		string= str(self.dist_left)+" "+str(self.dist_front)+" "+str(self.dist_right)+"\n"
 		if self.c<=(self.n_tokens*5):
 			self.f.write(string)
 		
-
+	#function to detect the distance of golden tokens ("range_front" and "range_lat" express 
+	#respectively the frontal and the lateral range in degrees that has to be detected) 
 	def find_obstacles(self,range_front=30,range_lat=[80,100]):
 		
 		self.dist_left=self.dist_right=self.dist_front= 100
@@ -41,6 +47,8 @@ class Analiser:
 				
 		return self.dist_front,self.dist_left,self.dist_right
 	
+	#function to write the timestamp of each grabbed token to a .txt file. 
+	#("start", "end" must be "time.time()" instances of the Time class)
 	def collect_lap_time(self,start,end):
 		if self.c<=self.n_tokens*3:
 			self.f2.write(str(end-start)+"\n")
